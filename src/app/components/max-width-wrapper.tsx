68829c9594818d75a2cf6411d7134ae894ef5c60
Justin Rocket <jrman28@gmail.com>
2024-11-15 05:55:10 +0000

    create maxwidthwrapper

--- src/app/components/max-width-wrapper.tsx
+++ src/app/components/max-width-wrapper.tsx
@@ -1,1 +1,7 @@
-export const MaxWidthWrapper
+interface MaxWidthWrapperProps {
+    className?: string
+}
+
+export const MaxWidthWrapper = ({className, children}: MaxWidthWrapperProps) => {
+    return <div></div>
+}
